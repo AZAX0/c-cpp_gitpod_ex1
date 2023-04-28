a4d9cf7d534bb28f160cab6d9c699b013402c042
AZAX0 <131453845+AZAX0@users.noreply.github.com>
2023-04-28 20:37:23 +0800

    Update ex2-3.cpp

--- ex2-3.cpp
+++ ex2-3.cpp
@@ -1,6 +1,8 @@
 #include <stdio.h> 
 int main(void) {
      int height_m = 1, height_cm =88;
+     float f_height = 1.80;
+     char a = 'b';
      printf(" hello azax ");
      printf(" my height is %d m %d cm \n",height_m,height_cm);
 }
